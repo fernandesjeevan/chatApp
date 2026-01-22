df029345109d47af44126f05bedfab4416243f50
Jeevan Fernandes <Jeevan.Fernandes@novigo.com>
2026-01-22 16:13:12 +0530

    feat-added engines greater than 20 for package json

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ app.use("/api/auth",authRoutes)
 app.use("/message", messageRoutes)
 //make ready for deployment
 if(process.env.NODE_ENV ==="production"){
-    console.log("helio")
+   
     app.use(express.static(path.join(__dirname,"../frontend/dist")));
     app.get("*",(req,res)=>{
         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
