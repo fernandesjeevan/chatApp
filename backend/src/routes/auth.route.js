e1c6cddfc40510972767c9a5b8e3f080ae3ba8d6
Jeevan Fernandes <jeevanfernandes111@gmail.com>
2026-01-27 15:16:44 +0530

    (feat) login and logout done

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,15 +1,10 @@
 import express from "express"
-import {signup} from "../controllers/auth.controller.js"
+import {logout, signup,login} from "../controllers/auth.controller.js"
 const router = express.Router();
-router.get("/signup",signup);
+router.post("/signup",signup);
 
-router.get("/login",(req,res)=>{
-    res.send("login endpoint");
+router.post("/login", login);
 
-});
-
-router.get("/logout",(req,res)=>{
-    res.send("Logout endpoint");
-});
+router.post("/logout",logout);
 
 export default router;
