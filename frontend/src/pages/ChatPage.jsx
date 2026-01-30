300ba80a2f3b082560b7b98e6b3e9da0fc69d14e
Jeevan Fernandes <jeevanfernandes111@gmail.com>
2026-01-30 15:22:05 +0530

    login logout implemented

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useAuthStore } from '../store/useAuthStore'
 
 function ChatPage() {
+  const {logout,isLoggingOut} = useAuthStore();
   return (
-    <div>
+    <div className="z-10  ">
       <h1> chatpage</h1>
+      <button onClick={logout}> logout</button>
     </div>
   )
 }
