bda80c7b69c300ad22321f05a376e21c1f580095
Jeevan Fernandes <jeevanfernandes111@gmail.com>
2026-02-02 18:19:01 +0530

    feat end of day

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,6 +1,45 @@
-import React from 'react'
-
+import useKeyboardSound from "../hooks/useKeyboardSound"
+import { useChatStore } from "../store/useChatStore";
 function MessageInput() {
+  
+  const {playRandomKeyStrokeSound} = useKeyboardSound();
+  const [text,setText] = useState("");
+  const [imagePreview,setImagePreview] = useState(null)
+
+  const fileInputRef =useRef(null);
+  const {sendMessage,isSoundEnabled} = useChatStore()
+    
+  const handleSendMessage = (e) =>{
+    e.preventDefault()
+    if(!text.trim() && !imagePreview) return;
+    if(isSoundEnabled) playRandomKeyStrokeSound()
+    sendMessage({
+        text: text.trim(),
+        image: imagePreview
+
+    })
+    setText("")
+    setImagePreview("")
+    if(fileInputRef.current) fileInputRef.current.value="";
+
+    const handleImageChange = (e) =>{
+        const file = e.target.files[0];
+        if(!file.type.startsWith("image/")){
+        toast.error("Please select an image file");
+        return;
+    }
+    const reader = new FileReader();
+    reader.onloadend = () => setImagePreview(reader.result);
+    reader.readAsDataURL(file);
+    };
+    const removeImage = () =>{
+        setImagePreview(null);
+        if(fileInputRef.current) fileInputRef.current.value ="";
+        
+    }
+    
+
+  };
   return (
     <div>
       
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -54,4 +54,14 @@ export const useChatStore = create((set,get)=>({
     }
   },
 
+  sendMessage: async(messageData) =>{
+    const {selectedUser,messages} =get()
+    try{
+        const res = await axiosInstance(`/messages/send/${selectedUser._id}`,messageData)
+        set({messages: messages.concat(res.data)})
+    }catch(error){
+        toast.error(error.response?.data?.message || "Something went wrong")
+    }
+  },
+
 }))
