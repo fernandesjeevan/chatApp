550188e666c3c72eb98da515c81db3750306bb4d
Jeevan Fernandes <jeevanfernandes111@gmail.com>
2026-02-02 12:02:35 +0530

    Reapply "sidebar related changes"
    This reverts commit a22b1e3f192d0555f07ddbd491c65b4b521eac8f.

--- frontend/src/components/ActiveTabSwitch.jsx
+++ frontend/src/components/ActiveTabSwitch.jsx
@@ -6,8 +6,12 @@ function ActiveTabSwitch() {
     <div className='tabs tabs-boxed bg-transparent p-2 m-2'>
       <button onClick={()=> setActiveTab("chats")}
       className={`tab ${
-        activeTab ==="chats"? "bg-cyan-500/20
-      }`}
+        activeTab ==="chats"? "bg-cyan-500/20 text-cyan-400": "text-slate-400"
+      }`}>Chats</button>
+      <button onClick={()=> setActiveTab("contacts")}
+      className={`tab ${
+        activeTab ==="contacts"? "bg-cyan-500/20 text-cyan-400": "text-slate-400"
+      }`}>Contacts</button>
     </div>
   )
 }
---
+++ frontend/src/components/ContactList.jsx
@@ -0,0 +1,40 @@
+    import React from 'react'
+    import { useEffect } from 'react';
+    import {useChatStore} from "../store/useChatStore.js"
+    import UsersLoadingSkeleton from './UsersLoadingSkeleton';
+    import NoChatsFound from './NoChatsFound.jsx';
+
+    function ContactsList() {
+    const {getAllContacts, allContacts, isUsersLoading, setSelectedUser} = useChatStore();
+    useEffect(()=>{
+        getAllContacts()
+    },[getAllContacts])
+
+    if(isUsersLoading) return <UsersLoadingSkeleton/>
+    if(allContacts.length===0) return <NoChatsFound/>
+    console.log(allContacts,"this is allContacts") 
+    console.log('we came here')
+    return (
+        <>
+        {allContacts.map(contact=>(
+        <div key={contact._id}
+            className='bg-cyan-500/10 p-4 rounded-lg cursor-pointer hover:bg-cyan-500/20 transition-colors'
+            onClick={()=>setSelectedUser(contact)}
+        >
+        <div className='flex items-center gap-3'>
+            {/* todo filx this online status with websocket */}
+            <div className="avatar online">
+            <div className="size-12 rounded-full">
+                <img src={contact.profilePic || "/avatar.png"} alt = {contact.fullName}/>
+            </div>
+
+            </div>
+            <h4 className='text-slate-200 font-medium truncate'>{contact.fullName}</h4>
+        </div>
+        </div>
+        ))}
+        </>
+    )
+    }  
+    
+    export default ContactsList
