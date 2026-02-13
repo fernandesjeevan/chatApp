6f6754ab45a3669c182584fa7740c3e3877b8277
Jeevan Fernandes <jeevanfernandes111@gmail.com>
2026-02-13 09:43:52 +0530

    css fixes

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -11,7 +11,7 @@ import ProfileHeader from "../components/ProfileHeader.jsx"
 function ChatPage() {
   const { activeTab ,selectedUser} = useChatStore();
   return (
-    <div className="relative w-full max-w-6xl h-[800px]">
+    <div className="relative w-full max-w-6xl box box-border h-full">
       <BorderAnimatedContainer>
         {/*LEFT SIDE */}
         <div className='w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col'>
